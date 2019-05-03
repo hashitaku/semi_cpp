d1bdcb27c54bff7b5cb3019960f19a2f6a04c4a4
hashitaku <s317062@ed.sus.ac.jp>
2019-05-04 07:07:40 +0900

    add file math_const.hpp

---
+++ math/math_const.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file math_const.hpp
+ * @brief mathematical constant
+ * @author hashitaku
+ * @data 2019/1/28
+ */
+
+#ifndef SEMI_CPP_MATH_CONST_HPP
+#define SEMI_CPP_MATH_CONST_HPP
+#include"./exp.hpp"
+
+namespace semi_cpp::math{
+/**
+ * @struct math_const
+ * @brief template struct math_const
+ * @details 数学定数を定義
+ */
+template<typename Type>
+struct math_const{
+    static constexpr Type e = small_tmp::exp(static_cast<Type>(1));
+    static constexpr Type pi = 3.14159265358979323846;
+};
+
+}
+
+#endif //SEMI_CPP_MATH_CONST_HPP
