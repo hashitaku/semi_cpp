61f9470280ac80b2a1af3f956c7fae083b0c5acf
hashitaku <s317062@ed.sus.ac.jp>
2019-05-04 23:12:00 +0900

    fix diffirential.hpp
    特殊化の条件を変更

--- numeric/diffirential.hpp
+++ numeric/diffirential.hpp
@@ -8,14 +8,15 @@
 #ifndef SEMI_CPP_NUMERIC_DIF
 #define SEMI_CPP_NUMERIC_DIF
 
-#include<complex>
+#include"./complex.hpp"
 #include<type_traits>
 
 namespace semi_cpp::numeric{
 
 template<typename Type, typename FuncType>
 constexpr auto differential_function(size_t n, Type x, FuncType func, double delta = 0.01){
-    if constexpr((std::is_same_v<Type, double> || std::is_same_v<Type, long double>) && (std::is_same_v<std::invoke_result_t<FuncType, Type>, double> || std::is_same_v<std::invoke_result_t<FuncType, Type>, long double>)){
+    namespace ns = semi_cpp::numeric;
+    if constexpr((std::is_floating_point_v<Type>) && (std::is_floating_point_v<std::invoke_result_t<FuncType, Type>>)){
         if(n == 0){
             return func(x);
         }else if(n == 1){
@@ -23,7 +24,8 @@ constexpr auto differential_function(size_t n, Type x, FuncType func, double del
         }else{
             return (differential_function(n - 1.0, x - 2.0 * delta, func) - 8.0 * differential_function(n - 1.0, x - delta, func) + 8.0 * differential_function(n - 1.0, x + delta, func) - differential_function(n - 1.0, x + 2.0 * delta, func)) / (12.0 * delta);
         }
-    }else if constexpr((std::is_same_v<Type, std::complex<double>> || std::is_same_v<Type, std::complex<long double>>) && (std::is_same_v<std::invoke_result_t<FuncType, Type>, std::complex<double>> || std::is_same_v<std::invoke_result_t<FuncType, Type>, std::complex<long double>>)){
+    }else if constexpr((std::is_same_v<Type, ns::complex<double>> || std::is_same_v<Type, ns::complex<long double>>) &&
+                       (std::is_same_v<std::invoke_result_t<FuncType, Type>, ns::complex<double>> || std::is_same_v<std::invoke_result_t<FuncType, Type>, ns::complex<long double>>)){
         auto delta_x = delta * x;
         if(n == 0){
             return func(x);
@@ -33,7 +35,7 @@ constexpr auto differential_function(size_t n, Type x, FuncType func, double del
             return (differential_function(n - 1.0, x - 2.0 * delta_x, func) - 8.0 * differential_function(n - 1.0, x - delta_x, func) + 8.0 * differential_function(n - 1.0, x + delta_x, func) - differential_function(n - 1.0, x + 2.0 * delta_x, func)) / (12.0 * delta_x);
         }
     }else{
-        static_assert([](){ return false; }(), "function differential_function() only 'double' 'long double' 'std::complex<double>' 'std::complaex<long double>'");
+        static_assert([](){ return false; }(), "function differential_function() is only floating points");
     }
 }
 
