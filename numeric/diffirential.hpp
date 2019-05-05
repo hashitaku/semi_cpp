eed4275c4841c935c69ab5dcee7ec121a7c987b8
hashitaku <s317062@ed.sus.ac.jp>
2019-05-05 09:01:29 +0900

    fix file diffirential.hpp
    特殊化を修正

--- numeric/diffirential.hpp
+++ numeric/diffirential.hpp
@@ -16,16 +16,8 @@ namespace semi_cpp::numeric{
 template<typename Type, typename FuncType>
 constexpr auto differential_function(size_t n, Type x, FuncType func, double delta = 0.01){
     namespace ns = semi_cpp::numeric;
-    if constexpr((std::is_floating_point_v<Type>) && (std::is_floating_point_v<std::invoke_result_t<FuncType, Type>>)){
-        if(n == 0){
-            return func(x);
-        }else if(n == 1){
-            return (func(x - 2.0 * delta) - 8.0 * func(x - delta) + 8.0 * func(x + delta) - func(x + 2.0 * delta)) / (12.0 * delta);
-        }else{
-            return (differential_function(n - 1.0, x - 2.0 * delta, func) - 8.0 * differential_function(n - 1.0, x - delta, func) + 8.0 * differential_function(n - 1.0, x + delta, func) - differential_function(n - 1.0, x + 2.0 * delta, func)) / (12.0 * delta);
-        }
-    }else if constexpr((std::is_same_v<Type, ns::complex<double>> || std::is_same_v<Type, ns::complex<long double>>) &&
-                       (std::is_same_v<std::invoke_result_t<FuncType, Type>, ns::complex<double>> || std::is_same_v<std::invoke_result_t<FuncType, Type>, ns::complex<long double>>)){
+    if constexpr((std::is_floating_point_v<Type> || std::is_same_v<Type, ns::complex<double>> || std::is_same_v<Type, ns::complex<long double>>) &&
+                 (std::is_floating_point_v<std::invoke_result_t<FuncType, Type>> || std::is_same_v<std::invoke_result_t<FuncType, Type>, ns::complex<double>> || std::is_same_v<std::invoke_result_t<FuncType, Type>, ns::complex<long double>>)){
         auto delta_x = delta * x;
         if(n == 0){
             return func(x);
