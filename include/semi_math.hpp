180c8afdad512520b5cad761bba08ff3a1c5d66d
hashitaku <s317062@ed.sus.ac.jp>
2019-07-27 08:17:30 +0900

    add include file
    インクルードファイルを追加
    それに伴い入れ子の名前空間をsemi_cppへ引き上げ

---
+++ include/semi_math.hpp
@@ -0,0 +1,26 @@
+/**
+ * @file semi_math.hpp
+ * @brief semi_cpp math include file
+ * @author hashitaku
+ * @data 2019/7/27
+ */
+
+#ifndef SEMI_MATH_HPP
+#define SEMI_MATH_HPP
+
+#include"./../math/abs.hpp"
+#include"./../math/atan.hpp"
+#include"./../math/cos.hpp"
+#include"./../math/exp.hpp"
+#include"./../math/fmod.hpp"
+#include"./../math/log.hpp"
+#include"./../math/math_const.hpp"
+#include"./../math/pow.hpp"
+#include"./../math/sin.hpp"
+#include"./../math/tan.hpp"
+
+namespace semi_cpp{
+    inline namespace math{}
+}
+
+#endif //SEMI_MATH_HPP
