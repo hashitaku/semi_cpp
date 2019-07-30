310add8b2ed8afe689027d133d83c528ed9a1b36
hashitaku <s317062@ed.sus.ac.jp>
2019-07-30 14:07:22 +0900

    fix test
    テストケースなどの変更
    テストケース名はファイルごとに
    テスト名は引数など条件ごとに名前分けした

--- test/test_abs.cpp
+++ test/test_abs.cpp
@@ -2,13 +2,17 @@
 
 #include"../math/abs.hpp"
 
-TEST(SEMI_CPP_TEST, MATH_ABS){
+TEST(TestAbs, TestInt){
     constexpr auto test_int = semi_cpp::math::abs(-314);
     ASSERT_EQ(test_int, 314);
+}
 
+TEST(TestAbs, TestFloat){
+    constexpr auto test_float = semi_cpp::math::abs(-3.14);
+    ASSERT_FLOAT_EQ(test_float, 3.14f);   
+}
+
+TEST(TestAbs, TestDouble){
     constexpr auto test_double = semi_cpp::math::abs(-3.14);
     ASSERT_DOUBLE_EQ(test_double, 3.14);
-
-    constexpr auto test_float = semi_cpp::math::abs(-3.14);
-    ASSERT_FLOAT_EQ(test_float, 3.14f);
 }
